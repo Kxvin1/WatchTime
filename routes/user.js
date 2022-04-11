5982b9da655e32b3ad43b1a8da0ec2404adca653
Jingling Jin <jljin0524@gmail.com>
2022-04-11 17:34:44 -0500

    register part done

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-
+//Login Page
 router.get('/login', csrfProtection, asyncHandler(async (req, res, next) => {
   res.render('login', {
     title: "Login Page",
@@ -63,5 +63,87 @@ router.post('/login', csrfProtection, loginValidators, asyncHandler(async(req, r
     csrfToken: req.csrfToken(),
   });
 }));
+//Register Page
+router.get('/register', csrfProtection, (req, res) => {
+  const user = db.User.build();// initialize a new user to pass into the user-register view
+  res.render('register', {
+    title: 'Register',
+    user,
+    csrfToken: req.csrfToken(),
+  });
+});
+
+const userValidators = [
+  // TODO Define the user validators.
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for First Name')
+    .isLength({ max: 25 })
+    .withMessage('First Name must not be more than 25 characters long'),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 50 })
+    .withMessage('Email Address must not be more than 50 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account');
+          }
+        });
+    }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    })
+];
+
+router.post('/register', csrfProtection, userValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      username,
+      email,
+      password,
+    } = req.body;
+
+    const user = db.User.build({
+      username,
+      email,
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      const hashedPassword = await bcrypt.hash(password, 10);//
+      user.hashedPassword = hashedPassword;
+      await user.save();
+      loginUser(req, res, user);
+      res.redirect('/');
+
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('register', {
+        title: 'Register',
+        user,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  })
+);
+
 
 module.exports = router;
