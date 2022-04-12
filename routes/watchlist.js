9317cb11a776df5a2c5d5c8456244efe56e7bb45
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-04-12 18:59:52 -0400

    Post route for watchlists mostly complete

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -17,7 +17,7 @@ async function findShelf (watchStatus, userId) {
         }
     });
 
-    return shelf.Movies
+    return shelf
 }
 
 router.get('/', asyncHandler( async(req, res, next) => {
@@ -33,12 +33,30 @@ router.get('/', asyncHandler( async(req, res, next) => {
 
     res.render('watchlist', {
         username,
-        planToWatchMoviesShelf,
-        watchingMoviesShelf,
-        haveWatchedMoviesShelf,
+        planToWatchMoviesShelf: planToWatchMoviesShelf.Movies,
+        watchingMoviesShelf: watchingMoviesShelf.Movies,
+        haveWatchedMoviesShelf: haveWatchedMoviesShelf.Movies,
         watchStatus
     })
 }))
 
+router.post('/', csrfProtection, asyncHandler( async(req, res, next) => {
+    const { movieId, watchStatus } = req.body;
+    const userId = req.session.auth ? req.session.auth.userId : 1; // not passing because not logged in (need to create pug file and sign in?)
+    const shelf = await findShelf(userId, watchStatus);
+    console.log(shelf.id);
+
+    const watchlist = await db.WatchList.create({
+        shelfId: shelf.id,
+        movieId
+    })
+    // either redirect them to their watchlist or leave on movie page
+
+}))
+
+router.put('/:watchlistId', asyncHandler( async(req, res, next) => {
+    // update the watch status for the selected movie
+    // refresh the watchlist
+}))
 
 module.exports = router;
